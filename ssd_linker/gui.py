3545aa07efac1f560e4801b0efe5fc8b577fdaaa
Pavel Bondar <pasha117@gmail.com>
2015-07-20 22:21:35 +0300

    List drives on win32 platform
    Fixed building tree on win platform

--- ssd_linker/gui.py
+++ ssd_linker/gui.py
@@ -1,6 +1,5 @@
 import sys
 from PyQt4 import QtGui, uic
-#from PyQt4 import uic
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 
@@ -33,18 +32,33 @@ def addItems(parent, elements):
             addItems(item, children)
 
 import os
-def buildTree(dir, deep=3):
-    if os.path.isfile(dir) or not os.access(dir, os.R_OK) or deep == 0:
+def buildTree(dir, deep=1):
+    if not os.path.isdir(dir) or not os.access(dir, os.R_OK) or deep < 0:
         return []
-    return [(name, buildTree(dir + '/' + name, deep - 1))
+    return [(name, buildTree(os.path.join(dir, name), deep - 1))
             for name in os.listdir(dir)]
+
+def get_drives():
+    import win32api
+    drives = win32api.GetLogicalDriveStrings()
+    drives = drives.split('\000')[:-1]
+    print drives
+    return drives
+
+def list_dirs(deep=1):
+    if sys.platform == "win32":
+        drives = get_drives()
+        return [(name, buildTree(name, deep - 1))
+                for name in drives]
+    else:
+        return buildTree('/', deep)
 
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
     window = MyWindow()
 
     model = QStandardItemModel()
-    tree = buildTree('/home')
+    tree = list_dirs()
     print tree
     addItems(model, tree)
     window.treeView.setModel(model)
